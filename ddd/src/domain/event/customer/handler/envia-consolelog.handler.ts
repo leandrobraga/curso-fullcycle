cbd87b6b0aad177d57160e50370ca5b56d864ebb
Leandro Braga <leandro.quadros@gmail.com>
2023-06-12 18:01:46 -0300

    Add event to customer

---
+++ ddd/src/domain/event/customer/handler/envia-consolelog.handler.ts
@@ -0,0 +1,8 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class EnviaConsolelog implements EventHandlerInterface<CustomerCreatedEvent> {
+    handle(event: CustomerCreatedEvent): void {
+        console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.Address.toString()}`);
+    }
+}

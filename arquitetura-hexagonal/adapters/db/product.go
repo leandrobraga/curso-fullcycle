875e853245a99cfc525e37c0990b25287da51fef
Leandro Braga <leandro.quadros@gmail.com>
2023-06-30 16:01:20 -0300

    Test services in main

--- arquitetura-hexagonal/adapters/db/product.go
+++ arquitetura-hexagonal/adapters/db/product.go
@@ -69,7 +69,7 @@ func (p *ProductDb) update(product application.ProductInterface) (application.Pr
 
 func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
 	var rows int
-	p.db.QueryRow(`SELECT id from products where id=?`, product.GetID()).Scan(rows)
+	p.db.QueryRow(`SELECT count(*) from products where id=?`, product.GetID()).Scan(&rows)
 	if rows == 0 {
 		_, err := p.create(product)
 		if err != nil {

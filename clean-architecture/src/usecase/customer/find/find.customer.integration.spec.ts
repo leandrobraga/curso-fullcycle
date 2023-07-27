4c72a77e682e153f8d2dd771ed1ddfa628eb0d34
Leandro Braga <leandro.quadros@gmail.com>
2023-07-26 22:27:17 -0300

    Unit test find customer usecase

--- clean-architecture/src/usecase/customer/find/find.customer.integration.spec.ts
+++ clean-architecture/src/usecase/customer/find/find.customer.integration.spec.ts
@@ -6,20 +6,6 @@ import Address from "../../../domain/customer/value-object/address";
 import FindCustomeruseCase from "./find.customer.usecase";
 
 
-const customer = new Customer("123", "Jhon");
-const address = new Address("Rua tal", 123, "39400-123", "Moc");
-customer.changeAddress(address);
-
-const MockRepository = () => {
-  return {
-    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
-    findAll: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-  }
-}
-
-
 describe("Test find customer use case", () => {
   let sequelize: Sequelize;
 
@@ -40,9 +26,14 @@ describe("Test find customer use case", () => {
   });
 
   it("should find a customer", async () => {
-    const customerRepository = MockRepository();
+    const customerRepository = new CustomerRepository();
     const usecase = new FindCustomeruseCase(customerRepository);
-  
+
+    const customer = new Customer("123", "Jhon");
+    const address = new Address("Rua tal", 123, "39400-123", "Moc");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
     const input = {
         id : "123"
     }
@@ -61,20 +52,5 @@ describe("Test find customer use case", () => {
     const result = await usecase.execute(input)
 
     expect(result).toEqual(output);
-  });
-
-  it("should not found a customer", () => {
-    const customerRepository = MockRepository();
-    customerRepository.find.mockImplementation(() => {
-      throw new Error("Customer not found");
-    });
-    const usecase = new FindCustomeruseCase(customerRepository);
-
-    const input = {
-      id : "123"
-    };
-    expect(() => {
-      return usecase.execute(input);
-    }).rejects.toThrow("Customer not found")
-  });
+  })
 });

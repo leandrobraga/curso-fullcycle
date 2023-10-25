697ec7540d2157cfed927ee94c4ba2876c16fd56
Leandro Braga <leandro.quadros@gmail.com>
2023-10-25 22:55:42 +0100

    Add Notification pattern in products and tests

--- clean-architecture/src/domain/product/entity/product.spec.ts
+++ clean-architecture/src/domain/product/entity/product.spec.ts
@@ -16,6 +16,11 @@ describe("Product Unit Tests", ()=>{
             let product = new Product("123", "Product 1", -1);
         }).toThrowError("Price must be greater than zero");
     });
+    it("should throw error when id is empty and price less than zero", () => {
+        expect(() => {
+            let product = new Product("", "Product 1", -1);
+        }).toThrowError("product: Id is required,product: Price must be greater than zero");
+    });
     it("should change name", () => {
         const product = new Product("123", "Product 1", 100);
         product.changeName("Product 2");

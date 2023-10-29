65efbad37d9e3bf73787c877bb19fe6dfb393db4
Leandro Braga <leandro.quadros@gmail.com>
2023-10-29 18:28:17 +0000

    VAlidator product

--- clean-architecture/src/domain/product/validator/product.yup.validator.ts
+++ clean-architecture/src/domain/product/validator/product.yup.validator.ts
@@ -5,9 +5,31 @@ import * as yup from "yup";
 export default class ProductYupValidator implements ValidatorInterface<Product> {
     validate(entity: Product): void {
         try{
-            you'
+            yup
+            .object()
+            .shape({
+                id: yup.string().required("Id is required"),
+                name: yup.string().required("Name is required"),
+                price: yup.number().min(1, "Price must be greater than zero"),
+            }).validateSync(
+                {
+                    id: entity.id,
+                    name: entity.name,
+                    price: entity.price
+                },
+                {
+                    abortEarly: false,
+                }
+            );
         }catch (errors) {
-
+            const e = errors as yup.ValidationError;
+            e.errors.forEach(error => {
+                console.log(error);
+                entity.notification.addError({
+                    context: "product",
+                    message: error
+                });
+            });
         }
     }
 }
